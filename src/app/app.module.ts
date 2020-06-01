e61ec24d28e107943e1bb860719e57e19c7a2a1c
pamidisetti manikanta <pamidisetti_mk@yahoo.in>
2020-06-01 20:08:10 +0530

    latest wireframe like design and no service integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { UsersigninComponent } from './usersignin/usersignin.component';
+import { PostbarComponent } from './postbar/postbar.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    UsersigninComponent,
+    PostbarComponent,
+    LandingpageComponent,
+    PagenotfoundComponent,
+    UserdashboardComponent
   ],
   imports: [
     BrowserModule,
